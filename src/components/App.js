f37dae210c9b4a6a2a67dfa97db226a851c09912
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-03 13:39:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import '../styles/App.css';
+import {useState} from 'react'
 const App = () => {
+const[state,setState]=useState(1)
+const[color,setColor]=useState('normal')
+const Increment=()=>{
+setState(state+1)
+if(state%3==0){
+  setColor('fizz')
+}
+else if(state%5==0){
+  setColor('buzz')
+}
+else if(state%3==0&&state%5==0){
+  setColor('fizzbuzz')
+}
+else{
+  setColor('normal')
+}
+}
+const decrement=()=>{
+  setState(state-1)
+  if(state%3==0){
+    setColor('fizz')
+  }
+  else if(state%5==0){
+    setColor('buzz')
+  }
+  else if(state%3==0&&state%5==0){
+    setColor('fizzbuzz')
+  }
+  else{
+    setColor('normal')
+  }
+}
 
   
   return (
     <div id="main">
-
+      
+      <button onClick={Increment}>Increment</button>
+      <div id="counter" className={color}>{state}</div>
+      <button onClick={decrement}>Decrement</button>
     </div>
   )
 }
