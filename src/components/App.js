e43af4df1dd62c182be24480d09d1e629cf77c45
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-03 13:45:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,44 +3,33 @@ import '../styles/App.css';
 import {useState} from 'react'
 const App = () => {
 const[state,setState]=useState(1)
-const[color,setColor]=useState('normal')
+// const[color,setColor]=useState('normal')
 const Increment=()=>{
 setState(state+1)
-if(state%3==0){
-  setColor('fizz')
-}
-else if(state%5==0){
-  setColor('buzz')
-}
-else if(state%3==0&&state%5==0){
-  setColor('fizzbuzz')
-}
-else{
-  setColor('normal')
-}
+
 }
 const decrement=()=>{
   setState(state-1)
-  if(state%3==0){
-    setColor('fizz')
-  }
-  else if(state%5==0){
-    setColor('buzz')
-  }
-  else if(state%3==0&&state%5==0){
-    setColor('fizzbuzz')
-  }
-  else{
-    setColor('normal')
-  }
+
 }
+const getCounterClass = () => {
+  if (state % 3 === 0 && state % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (state % 3 === 0) {
+    return 'fizz';
+  } else if (state % 5 === 0) {
+    return 'buzz';
+  } else {
+    return 'normal';
+  }
+};
 
   
   return (
     <div id="main">
       
       <button onClick={Increment}>Increment</button>
-      <div id="counter" className={color}>{state}</div>
+      <div id="counter" className={getCounterClass()}>{state}</div>
       <button onClick={decrement}>Decrement</button>
     </div>
   )
