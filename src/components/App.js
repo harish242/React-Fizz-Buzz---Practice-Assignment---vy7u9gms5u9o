c4f91fc0b7e00f02b291bf534a2d4199d408746c
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-06 19:06:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,11 @@ const App = () => {
 const[state,setState]=useState(1)
 // const[color,setColor]=useState('normal')
 const Increment=()=>{
-setState(state+1)
+setState(state=>state+1)
 
 }
 const decrement=()=>{
-  setState(state-1)
+  setState(state=>state-1)
 
 }
 const getCounterClass = () => {
@@ -29,7 +29,7 @@ const getCounterClass = () => {
     <div id="main">
       
       <button onClick={Increment}>Increment</button>
-      <div id="counter" className={getCounterClass}>{state}</div>
+      <div id="counter" className={getCounterClass()}>{state}</div>
       <button onClick={decrement}>Decrement</button>
     </div>
   )
